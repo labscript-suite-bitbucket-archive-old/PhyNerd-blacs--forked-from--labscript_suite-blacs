0eaa12cc6ea9cf8105e32742610e6426a091de92
Jan Werkmann <jan.wrk.fb@gmail.com>
2018-03-16 12:14:01 +0100

    Added termination to all topics.

--- device_base_class.py
+++ device_base_class.py
@@ -278,7 +278,7 @@ class DeviceTab(Tab):
             properties.setdefault('parent',None)
             if hardware_name in self._AI:
                 widgets[hardware_name] = self._AI[hardware_name].create_widget(properties['display_name'],properties['horizontal_alignment'],properties['parent'])
-                self.socket.setsockopt(zmq.SUBSCRIBE, "{} {}".format(self.device_name, hardware_name).encode('utf-8'))
+                self.socket.setsockopt(zmq.SUBSCRIBE, "{} {}\0".format(self.device_name, hardware_name).encode('utf-8'))
 
         self.analog_in_thread = threading.Thread(target=self._analog_read_loop, args=(widgets,))
         self.analog_in_thread.daemon = True
@@ -290,7 +290,7 @@ class DeviceTab(Tab):
         while True:
             try:
                 devicename_and_channel, data = self.socket.recv_multipart()
-                channel = devicename_and_channel.decode().split(" ", 1)[1]
+                channel = devicename_and_channel.decode().split(" ", 1)[1][:-1]
                 data = np.frombuffer(memoryview(data), dtype=np.float64)
                 random_value = np.random.choice(data,1)[0]
                 widgets[channel].set_value(random_value)
